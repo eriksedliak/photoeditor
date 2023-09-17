6fe0608c2a58cae23dcc32e9020286b585452cf5
eriksedliak <eriksedliak@gmail.com>
2023-09-17 20:36:19 +0200

    Stage1 - Add image picker

--- app/src/main/java/com/eriks/photoeditor/MainActivity.kt
+++ app/src/main/java/com/eriks/photoeditor/MainActivity.kt
@@ -1,11 +1,79 @@
 package com.eriks.photoeditor
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var currentImage: ImageView
+    private lateinit var pickImageBtn: Button
+    private val activityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val photoUri = result.data?.data ?: return@registerForActivityResult
+                currentImage.setImageURI(photoUri)
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bindViews()
+
+        currentImage.setImageBitmap(createBitmap())
+        pickImageBtn.setOnClickListener {
+            pickImage()
+        }
+    }
+
+    private fun bindViews() {
+        currentImage = findViewById(R.id.ivPhoto)
+        pickImageBtn = findViewById(R.id.btnGallery)
+    }
+
+    private fun pickImage() {
+        val pickImageIntent =
+            Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        activityResultLauncher.launch(pickImageIntent)
+    }
+
+
+    // do not change this function
+    fun createBitmap(): Bitmap {
+        val width = 200
+        val height = 100
+        val pixels = IntArray(width * height)
+        // get pixel array from source
+
+        var R: Int
+        var G: Int
+        var B: Int
+        var index: Int
+
+        for (y in 0 until height) {
+            for (x in 0 until width) {
+                // get current index in 2D-matrix
+                index = y * width + x
+                // get color
+                R = x % 100 + 40
+                G = y % 100 + 80
+                B = (x + y) % 100 + 120
+
+                pixels[index] = Color.rgb(R, G, B)
+
+            }
+        }
+        // output bitmap
+        val bitmapOut = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+        bitmapOut.setPixels(pixels, 0, width, 0, 0, width, height)
+        return bitmapOut
     }
 }
