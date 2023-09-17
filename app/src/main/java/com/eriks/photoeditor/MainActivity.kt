adfb3ddf08618af4837904364305b08d8d6754f7
eriksedliak <eriksedliak@gmail.com>
2023-09-17 20:42:31 +0200

    Stage2 - Add brightness slider

--- app/src/main/java/com/eriks/photoeditor/MainActivity.kt
+++ app/src/main/java/com/eriks/photoeditor/MainActivity.kt
@@ -4,22 +4,31 @@ import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Color
+import android.graphics.ImageDecoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.ColorInt
+import com.google.android.material.slider.Slider
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var currentImage: ImageView
     private lateinit var pickImageBtn: Button
+    private lateinit var brightnessSl: Slider
+    private lateinit var currentBitmap: Bitmap
+
     private val activityResultLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
                 val photoUri = result.data?.data ?: return@registerForActivityResult
                 currentImage.setImageURI(photoUri)
+                currentBitmap = ImageDecoder.decodeBitmap(
+                    ImageDecoder.createSource(contentResolver, photoUri)
+                ).copy(Bitmap.Config.RGB_565, true)
             }
         }
 
@@ -28,15 +37,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         bindViews()
 
-        currentImage.setImageBitmap(createBitmap())
+        //do not change this line
+        currentBitmap = createBitmap()
+        currentImage.setImageBitmap(currentBitmap)
+
         pickImageBtn.setOnClickListener {
             pickImage()
+        }
+        brightnessSl.addOnChangeListener { _, value, _ ->
+            redrawPicture(currentBitmap, value.toInt())
         }
     }
 
     private fun bindViews() {
         currentImage = findViewById(R.id.ivPhoto)
         pickImageBtn = findViewById(R.id.btnGallery)
+        brightnessSl = findViewById(R.id.slBrightness)
+    }
+
+    private fun limitToRgb(color: Int, value: Int): Int {
+        return if (color + value < 0) 0
+        else if (color + value > 255) 255
+        else color + value
     }
 
     private fun pickImage() {
@@ -45,6 +67,27 @@ class MainActivity : AppCompatActivity() {
         activityResultLauncher.launch(pickImageIntent)
     }
 
+    private fun redrawPicture(pictureToRedraw: Bitmap, brightnessChange: Int) {
+        val height = pictureToRedraw.height
+        val width = pictureToRedraw.width
+        val editedImage = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+//        val limitColorToRgbRange = { color: Int, value: Int -> (color + value).coerceIn(0..255) }
+        @ColorInt val pixels = IntArray(height * width)
+
+        //brightness = R+G+B/3, simply add or subtract the same amount from each RGB color.
+        pictureToRedraw.getPixels(pixels, 0, width, 0, 0, width, height)
+        pixels.indices.forEach {
+//            val red = limitColorToRgbRange(Color.red(pixels[it]), brightnessChange)
+//            val green = limitColorToRgbRange(Color.green(pixels[it]), brightnessChange)
+//            val blue = limitColorToRgbRange(Color.blue(pixels[it]), brightnessChange)
+            val red = limitToRgb(Color.red(pixels[it]), brightnessChange)
+            val green = limitToRgb(Color.green(pixels[it]), brightnessChange)
+            val blue = limitToRgb(Color.blue(pixels[it]), brightnessChange)
+            pixels[it] = Color.rgb(red, green, blue)
+        }
+        editedImage.setPixels(pixels, 0, width, 0, 0, width, height)
+        currentImage.setImageBitmap(editedImage)
+    }
 
     // do not change this function
     fun createBitmap(): Bitmap {
